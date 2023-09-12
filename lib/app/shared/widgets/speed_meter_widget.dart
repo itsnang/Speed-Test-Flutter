b0aac683dc96665d03a26bd9b59d2a4f0a54e785
itsnang <lorn.samnang@cellcard.com.kh>
2023-09-12 12:41:02 +0700

    feat: speedmeter widget

---
+++ lib/app/shared/widgets/speed_meter_widget.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:speedtest_kh/app/shared/themes/theme.dart';
+import 'package:speedtest_kh/app/shared/widgets/text_widget.dart';
+import 'package:syncfusion_flutter_gauges/gauges.dart';
+
+class SpeedMeter extends StatelessWidget {
+  const SpeedMeter({
+    super.key,
+    required this.currentSpeed,
+  });
+
+  final double currentSpeed;
+
+  @override
+  Widget build(BuildContext context) {
+    return SfRadialGauge(
+      enableLoadingAnimation: true,
+      animationDuration: 3000,
+      axes: <RadialAxis>[
+        RadialAxis(
+          showTicks: false,
+          labelOffset: 30,
+          showLastLabel: true,
+          maximum: 110,
+          startAngle: 150,
+          endAngle: 30,
+          ranges: [
+            GaugeRange(
+              rangeOffset: -1,
+              startValue: 0,
+              endValue: 110,
+              color: context.disableColor,
+              startWidth: 20,
+              endWidth: 20,
+            ),
+          ],
+          showAxisLine: false,
+          pointers: <GaugePointer>[
+            NeedlePointer(
+              enableAnimation: true,
+              value: currentSpeed,
+              needleColor: context.primaryColor,
+              knobStyle: KnobStyle(
+                color: context.primaryColor,
+                knobRadius: 0.09,
+                borderWidth: 0.01,
+                borderColor: context.primaryColor,
+              ),
+              needleEndWidth: 16.1,
+              needleLength: 0.66,
+              animationType: AnimationType.elasticOut,
+            ),
+            RangePointer(
+              value: currentSpeed,
+              width: 0.12,
+              sizeUnit: GaugeSizeUnit.factor,
+              color: context.primaryColor,
+            ),
+          ],
+          annotations: <GaugeAnnotation>[
+            GaugeAnnotation(
+              widget: TextWidget.subtitle(
+                color: context.primaryColor,
+                isBold: true,
+                '${currentSpeed.toStringAsFixed(1)} Mbps',
+              ),
+              angle: 90,
+              positionFactor: 0.5,
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
